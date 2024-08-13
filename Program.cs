14c689ad2e44569ed630a5e85e85602053818ed9
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-08-13 21:36:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 0;
+            for (; i <= 10; i++)
+            {
+                for (; j <= i; j++)
+                {
+                    Console.Write(j);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
